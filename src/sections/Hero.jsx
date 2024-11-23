ba3c3f85c2fc6a2aa9cc1aef7930bfb0a7306f03
IsuruSH <shanakaisuru11@gmail.com>
2024-11-24 01:15:04 +0530

    add Hero section to App component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,13 @@
-import React from "react";
-
 const Hero = () => {
-  return <div></div>;
+  return (
+    <section className="min-h-screen w-full flex flex-col relative">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt20 gap-3">
+        <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
+          Hi, I am Isuru <span className="waving-hand">👋</span>
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
