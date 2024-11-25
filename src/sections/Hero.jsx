d5b7ea2c6bca1430856e8289d4d2966fea8ee379
IsuruSH <shanakaisuru11@gmail.com>
2024-11-25 23:53:54 +0530

    add CanvasLoader component and integrate it into Hero section with suspense fallback

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,8 @@
 import { PerspectiveCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import HackerRoom from "../components/HackerRoom";
+import { Suspense } from "react";
+import CanvasLoader from "../components/CanvasLoader";
 
 const Hero = () => {
   return (
@@ -15,8 +17,16 @@ const Hero = () => {
       </div>
       <div className="w-full h-full absolute inset-0">
         <Canvas className="w-full h-full">
-          <PerspectiveCamera makeDefault position={[0, 0, 30]} />
-          <HackerRoom />
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+            <HackerRoom
+              scale={0.07}
+              position={[0, 0, 0]}
+              rotation={[0, 280, 0]}
+            />
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+          </Suspense>
         </Canvas>
       </div>
     </section>
